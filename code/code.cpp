8250416d8f4a4ed60656e7abe9adbc5646894393
Anurag Maurya <anuragmauryag@gmail.com>
2018-10-29 20:34:20 +0530

    done indentation

--- code/code.cpp
+++ code/code.cpp
@@ -19,6 +19,7 @@ void printall(int beg,vector<int> p[],int *a,int n,int idx,int *cur,int size)
 		printit(cur,size);
 		return;
 	}
+	
 	for(int i=0;i<p[beg].size();i++)
 	{
 		cur[size]=a[p[beg][i]];
@@ -34,7 +35,7 @@ void lis(int *a,int n)
 
 	for(int i=0;i<n;i++)
 	{
-        store[i]=1;
+        	store[i]=1;
 	}
 
 	for(int i=1;i<n;i++)
@@ -43,59 +44,59 @@ void lis(int *a,int n)
 		{
 			if(a[j]<a[i])
 			{
-                if(store[i]<(store[j]+1))
-                {
-                	store[i]=store[j]+1;
-                	while(parent[i].size())
-                	{
-                		parent[i].pop_back();
-                	}
-                	parent[i].push_back(j);
-                }
-                else if(store[i]==store[j]+1)
-                {
-                	parent[i].push_back(j);
-                }
+                		if(store[i]<(store[j]+1))
+                		{
+                			store[i]=store[j]+1;
+                			while(parent[i].size())
+                			{
+                				parent[i].pop_back();
+                			}
+                			parent[i].push_back(j);
+                		}
+                		else if(store[i]==store[j]+1)
+                		{
+                			parent[i].push_back(j);
+                		}
 			}
 		}
 	}
 
-    for(int i=0;i<n;i++)
-    {
-    	cout<<store[i]<<" ";
-    }
-    cout<<"\n";
+        for(int i=0;i<n;i++)
+    	{
+    		cout<<store[i]<<" ";
+    	}
+    	cout<<"\n";
 
 	int *cur=new int[n];
 	int mx=store[0],cnt=1;
 	vector<int> midx;
 	midx.push_back(0);
-  //finding all the candidate indexes for lis
+  	//finding all the candidate indexes for lis
 	for(int i=1;i<n;i++)
 	{
-        if(store[i]==mx)
-        {
-        	midx.push_back(i);
-            cnt++;
-        }
-        else if(store[i]>mx)
-        {
-        	mx=store[i];
-        	while(midx.size())
+        	if(store[i]==mx)
         	{
-        		midx.pop_back();
+        		midx.push_back(i);
+            		cnt++;
+        	}	
+        	else if(store[i]>mx)
+        	{
+        		mx=store[i];
+        		while(midx.size())
+        		{
+        			midx.pop_back();
+        		}
+        		midx.push_back(i);
+        		cnt=1;
         	}
-        	midx.push_back(i);
-        	cnt=1;
-        }
 	}
 
-    cout<<"count "<<cnt<<"\n";
-    for(int i=0;i<midx.size();i++)
-    {
-    	cur[0]=a[midx[i]];
-    	printall(midx[i],parent,a,n,0,cur,1);
-    }
+    	cout<<"count "<<cnt<<"\n";
+    	for(int i=0;i<midx.size();i++)
+    	{
+    		cur[0]=a[midx[i]];
+    		printall(midx[i],parent,a,n,0,cur,1);
+    	}
 }
 
 int main()
